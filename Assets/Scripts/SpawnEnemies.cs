6d5e7cfe3ffb0137aecc4369910779085c93fcdc
AshirAli <ashirmayankalathil@gmail.com>
2019-04-19 18:07:37 +0530

    Player settings updated

--- Assets/Scripts/SpawnEnemies.cs
+++ Assets/Scripts/SpawnEnemies.cs
@@ -27,13 +27,13 @@ public class SpawnEnemies : MonoBehaviour
 
     void SpawnEnemyAircrafts()
     {
-        //Vector3 pos =  new Vector3(Random.Range(-size.x/2, size.x / 2), Random.Range(-size.y / 2, size.y / 2), Camera.main.farClipPlane);
-        Vector3 pos = Camera.main.ScreenToWorldPoint(
+        Vector3 pos =  new Vector3(Random.Range(-size.x/2, size.x / 2), Random.Range(-size.y / 2, size.y / 2), Camera.main.farClipPlane);
+        /*Vector3 pos = Camera.main.ScreenToWorldPoint(
                             new Vector3(
                                 Random.Range(0, Screen.width), 
                                 Random.Range(0, Screen.height), 
                                 Camera.main.farClipPlane)
-                      );
+                      );*/
         //print(pos);
         GameObject EnemyInstance = Instantiate(EnemyAirCraft,pos,Quaternion.identity);
         //EnemyInstance.transform.Rotate(0, -180, 0);
